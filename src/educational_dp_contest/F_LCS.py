fcbdf475d64dd99c7806c70b29658a3b6efe46d0
kskkrt <kskkrt0326@gmail.com>
2024-02-12 03:24:06 +0900

    change dp-F

--- src/educational_dp_contest/F_LCS.py
+++ src/educational_dp_contest/F_LCS.py
@@ -5,10 +5,9 @@ dp = [[0 for _ in range(M + 1)] for _ in range(N + 1)]
 
 for i in range(1, N + 1):
     for j in range(1, M + 1):
+        dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
         if s[i - 1] == t[j - 1]:
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1] + 1)
-        else:
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+            dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1)
 
 i, j = N, M
 inv_result = []
